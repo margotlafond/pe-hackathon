5bcc6b3fc56273483b598ace3d4dc3656af6f116
Margot Lafond <margot.lafond@etu.minesparis.psl.eu>
2024-10-29 16:23:45 +0100

    Rayon de la planète en fonction de sa période orbitale

--- .ipynb_checkpoints/code_margot1-checkpoint.py
+++ .ipynb_checkpoints/code_margot1-checkpoint.py
@@ -14,17 +14,12 @@ plt.ylabel("Année de découverte")
 #Année de découverte en fonction de la distance de l'exoplanète à la Terre
 mask = (df['soltype'] == 'Published Confirmed')
 df1 = df[mask]
-plt.scatter(x = df1['sy_dist'], y = df1['disc_year'], marker = 'o')
+df2 = df1[['disc_year', 'sy_dist']]
+plt.scatter(x = df2['sy_dist'], y = df2['disc_year'], marker = 'o')
 plt.title("Année de découverte en fonction de la distance des exoplanètes confirmées à la Terre")
 plt.xlabel("Distance de l'exoplanète à la Terre (parsec)")
 plt.ylabel("Année de découverte")
 
-#Rayon de la planète en fonction de sa période orbitale
-plt.scatter(x = df1['pl_orbper'], y = df1['pl_rade']*6371000, marker = 'o')
-plt.title("Rayon de la planète en fonction de sa période orbitale")
-plt.xlabel("Période orbitale (jours)")
-plt.ylabel("Rayon de la planète (m)")
-plt.xscale('log')
-plt.yscale('log')
+
 
 
